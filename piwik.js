0a2d1f8ee3a3307f0f7d616444ac58e1f80b7fe4
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2015-02-24 10:50:30 -0800

    Create piwik.js

---
+++ piwik.js
@@ -0,0 +1,17 @@
+var _paq = _paq || [];
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="//piwik.dhimmel.com/";
+  _paq.push(['setTrackerUrl', u+'piwik.php']);
+  _paq.push(['setSiteId', 4]);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
+
+/* Add following html to pages on this domain
+<!-- Piwik -->
+</script src="//git.dhimmel.com/piwik.js">
+<noscript><p><img src="//piwik.dhimmel.com/piwik.php?idsite=4" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
+*/
